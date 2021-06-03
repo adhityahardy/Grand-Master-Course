828fc07bc63c1a6f33ccd350b599d2762346bc37
beginner75 <44922464+adhityahardy@users.noreply.github.com>
2021-06-03 23:01:19 +0700

    guru admin crud done

--- application/views/menuadmin/listguru.php
+++ application/views/menuadmin/listguru.php
@@ -27,6 +27,7 @@
                             <table id="example2" class="table table-bordered table-hover">
                                 <thead>
                                     <tr>
+                                        <th>No.</th>
                                         <th>Id Guru</th>
                                         <th>Nama Guru</th>
                                         <th>No. Hp</th>
@@ -39,9 +40,11 @@
                                 <tbody>
                                     <?php
                                     if ($list_data) {
+                                        $nomor = 1;
                                         foreach ($list_data as $guru) {
                                     ?>
                                             <tr>
+                                                <td><?= $nomor++ ?></td>
                                                 <td><?= $guru['idGuru'] ?></td>
                                                 <td><?= $guru['namaGuru'] ?></td>
                                                 <td><?= $guru['noHp'] ?></td>
@@ -49,7 +52,56 @@
                                                 <td><?= $guru['username'] ?></td>
                                                 <td><?= $guru['password'] ?></td>
                                                 <td>
-                                                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModalEdit"><i class="fas fa-edit"></i></button>
+                                                    <a type="button" id='btn-edit' class="btn btn-success" href="<?= base_url('admin/editGuru?id=' . $guru['idGuru']) ?>" data-toggle="modal" data-target="#exampleModalEdit<?php echo $guru['idGuru']; ?>"><i class=" fas fa-edit"></i></a>
+                                                    <!-- Modal Edit -->
+                                                    <div class="modal fade" id="exampleModalEdit<?php echo $guru['idGuru']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                        <div class="modal-dialog" role="document">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <h5 class="modal-title" id="exampleModalLabel">Edit Guru</h5>
+                                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                        <span aria-hidden="true">&times;</span>
+                                                                    </button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    <!-- Form Edit -->
+                                                                    <form method="POST" action="<?php echo base_url() . 'Admin/editGuru'; ?>">
+                                                                        <div class="form-group">
+                                                                            <label for="recipient-name" class="col-form-label">Id Guru:</label>
+                                                                            <input type="text" name="idGuru" class="form-control" value="<?= $guru['idGuru'] ?>" readonly>
+                                                                        </div>
+                                                                        <div class="form-group">
+                                                                            <label for="message-text" class="col-form-label">Nama:</label>
+                                                                            <input type="text" name="namaGuru" class="form-control" value="<?= $guru['namaGuru'] ?>" required>
+                                                                        </div>
+                                                                        <div class="form-group">
+                                                                            <label>No Hp:</label>
+                                                                            <input type="text" name="noHp" class="form-control" value="<?= $guru['noHp'] ?>" required>
+                                                                        </div>
+                                                                        <div class="form-group">
+                                                                            <label>Alamat:</label>
+                                                                            <input type="text" name="alamat" class="form-control" value="<?= $guru['alamat'] ?>" required>
+                                                                        </div>
+                                                                        <div class="form-group">
+                                                                            <label for="message-text" class="col-form-label">Username:</label>
+                                                                            <input type="text" name="username" class="form-control" value="<?= $guru['username'] ?>" required>
+                                                                        </div>
+                                                                        <div class="form-group">
+                                                                            <label for="message-text" class="col-form-label">New Password:</label>
+                                                                            <input type="text" name="password" class="form-control" required>
+                                                                        </div>
+
+                                                                        <div class="modal-footer">
+                                                                            <button type="reset" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                                                        </div>
+                                                                    </form>
+                                                                    <!-- Form Edit -->
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <!-- Modal Edit -->
                                                     <a type="button" class="btn btn-danger" href="<?= base_url('admin/deleteGuru?id=' . $guru['idGuru']) ?>" onclick="return confirm('Are You Sure?')"><i class="far fa-trash-alt"></i></a>
                                                 </td>
                                             </tr>
@@ -93,23 +145,23 @@
                     <!-- Form Create -->
                     <form method="post" action="<?php echo base_url() . 'Admin/createGuru'; ?>">
                         <div class="form-group">
-                            <label>Nama Guru</label>
+                            <label>Nama Guru:</label>
                             <input type="text" name="namaGuru" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label>No Hp</label>
-                            <input type="text" name="nohp" class="form-control">
+                            <label>No Hp:</label>
+                            <input type="text" name="noHp" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label>Alamat</label>
-                            <input type="text" name="alamat" class="form-control">
+                            <label>Alamat:</label>
+                            <input type="text" name="alamat" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label>Username</label>
+                            <label>Username:</label>
                             <input type="text" name="username" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label>Password</label>
+                            <label>Password:</label>
                             <input type="text" name="password" class="form-control" required>
                         </div>
                         <div class="modal-footer">
@@ -123,54 +175,6 @@
         </div>
     </div>
     <!-- Modal Create -->
-    <!-- Modal Edit -->
-    <div class="modal fade" id="exampleModalEdit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Edit Guru</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form method="post" action="<?php echo base_url() . 'Admin/editGuru'; ?>">
-                        <!-- Form Edit -->
-                        <div class="form-group">
-                            <label>Nama Guru</label>
-                            <input type="text" name="namaGuru" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label>No Hp</label>
-                            <input type="text" name="nohp" class="form-control">
-                        </div>
-                        <!-- <div class="form-group">
-                            <label>Email</label>
-                            <input type="text" name="email" class="form-control">
-                        </div> -->
-                        <div class="form-group">
-                            <label>Alamat</label>
-                            <input type="text" name="alamat" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label>Username</label>
-                            <input type="text" name="username" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label>Password</label>
-                            <input type="text" name="password" class="form-control" required>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="reset" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </div>
-                    </form>
-                    <!-- Form Edit -->
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Modal Edit -->
 
 </div>
 <!-- /.content-wrapper -->
--- application/views/menuadmin/listsiswa.php
+++ application/views/menuadmin/listsiswa.php
@@ -67,27 +67,27 @@
                                                                     <form method="post" action="<?php echo base_url() . 'Admin/editSiswa'; ?>">
                                                                         <!-- Form Edit -->
                                                                         <div class="form-group">
-                                                                            <label>Id Siswa</label>
+                                                                            <label>Id Siswa:</label>
                                                                             <input type="text" name="idSiswa" id="namaSiswaEdit" class="form-control" value="<?= $siswa['idSiswa'] ?>" readonly>
                                                                         </div>
                                                                         <div class="form-group">
-                                                                            <label>Nama Siswa</label>
+                                                                            <label>Nama Siswa:</label>
                                                                             <input type="text" name="namaSiswa" id="namaSiswaEdit" class="form-control" value="<?= $siswa['namaSiswa'] ?>" required>
                                                                         </div>
                                                                         <div class="form-group">
-                                                                            <label>No Hp</label>
+                                                                            <label>No Hp:</label>
                                                                             <input type="text" name="noHp" class="form-control" value="<?= $siswa['noHp'] ?>" required>
                                                                         </div>
                                                                         <div class="form-group">
-                                                                            <label>Alamat</label>
+                                                                            <label>Alamat:</label>
                                                                             <input type="text" name="alamat" class="form-control" value="<?= $siswa['alamat'] ?>" required>
                                                                         </div>
                                                                         <div class="form-group">
-                                                                            <label>Email</label>
+                                                                            <label>Email:</label>
                                                                             <input type="text" name="email" class="form-control" value="<?= $siswa['email'] ?>" required validate="required:true">
                                                                         </div>
                                                                         <div class="form-group">
-                                                                            <label>Username</label>
+                                                                            <label>Username:</label>
                                                                             <input type="text" name="username" class="form-control" value="<?= $siswa['username'] ?>" required>
                                                                         </div>
                                                                         <div class="form-group">
@@ -149,27 +149,27 @@
                     <!-- Form Create -->
                     <form method="post" action="<?php echo base_url() . 'Admin/createSiswa'; ?>">
                         <div class="form-group">
-                            <label>Nama Siswa</label>
+                            <label>Nama Siswa:</label>
                             <input type="text" name="namaSiswa" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label>No Hp</label>
-                            <input type="text" name="nohp" class="form-control">
+                            <label>No Hp:</label>
+                            <input type="text" name="noHp" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label>Alamat</label>
-                            <input type="text" name="alamat" class="form-control">
+                            <label>Alamat:</label>
+                            <input type="text" name="alamat" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label>Email</label>
+                            <label>Email:</label>
                             <input type="text" name="email" class="form-control" required validate="required:true">
                         </div>
                         <div class="form-group">
-                            <label>Username</label>
+                            <label>Username:</label>
                             <input type="text" name="username" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label>Password</label>
+                            <label>Password:</label>
                             <input type="text" name="password" class="form-control" required>
                         </div>
                         <div class="modal-footer">
