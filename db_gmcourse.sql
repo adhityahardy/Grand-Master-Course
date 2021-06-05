3a08035ef1b7a22b13b66c535052418766c1ecf8
akunGB123 <akun.gb123@gmail.com>
2021-06-05 17:42:37 +0700

    update db

--- db_gmcourse.sql
+++ db_gmcourse.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2021 at 07:02 PM
+-- Generation Time: Jun 05, 2021 at 12:41 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -39,8 +39,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`idAdmin`, `namaAdmin`, `username`, `password`) VALUES
-(3, 'admin', 'admin', '$2y$10$mzRmqugMtUDadnVnZsKtp.cCb9CQlZ73yWp4QzFLghkSe.IWo.UtG'),
-(4, 'admin', 'admin', '$2y$10$mzRmqugMtUDadnVnZsKtp.cCb9CQlZ73yWp4QzFLghkSe.IWo.UtG');
+(3, 'admin', 'admin', '$2y$10$mzRmqugMtUDadnVnZsKtp.cCb9CQlZ73yWp4QzFLghkSe.IWo.UtG');
 
 -- --------------------------------------------------------
 
@@ -54,7 +53,7 @@ CREATE TABLE `guru` (
   `noHp` varchar(20) NOT NULL,
   `alamat` varchar(255) NOT NULL,
   `username` varchar(100) NOT NULL,
-  `password` varchar(20) NOT NULL
+  `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -64,9 +63,8 @@ CREATE TABLE `guru` (
 INSERT INTO `guru` (`idGuru`, `namaGuru`, `noHp`, `alamat`, `username`, `password`) VALUES
 (1, 'Hadian Syafiar', '081273625123', 'Komplek Taman Melati 2 nomor 5', 'hadian1', '123456'),
 (2, 'Kusuma Ningrat', '081263532782', 'Jalan Merdeka nomor 3', 'kusuma1', '123456'),
-(3, 'Andi Bertoli', '081245672123', 'Jalan Kasuari nomor 5', 'andi1', '123456'),
-(4, 'Jimbon Mahardika', '081234234213', 'Jalan Kusuma Jaya nomor 6', 'jimbon1', '123456'),
-(27, 'asdasdas', 'asdasd', 'asdasd', 'asdasdasd', 'asdasdasdad');
+(3, 'Baringin', '081245672123', 'Jalan Kasuari nomor 5', 'andi1', 'asdasd'),
+(30, 'guru', '08123123', 'disini', 'guru', '$2y$10$mBzlJn9B2r9WgkzpTESe6.1PJhKZjaauAxdjBXvC0hWQrehW6ocT6');
 
 -- --------------------------------------------------------
 
@@ -91,12 +89,9 @@ CREATE TABLE `jadwal` (
 --
 
 INSERT INTO `jadwal` (`idJadwal`, `tanggalJadwal`, `jamJadwal`, `idMatpel`, `idGuru`, `idSiswa`, `namaMatpel`, `namaGuru`, `namaSiswa`) VALUES
-(1, '0000-00-00', '15:00:00.0', 1, 1, 1, 'Biologi', 'Hadian Syafiar', 'siswa1'),
-(2, '0000-00-00', '14:00:00.0', 2, 2, 1, 'Matematika', 'Kusuma Ningrat', 'siswa1'),
-(3, '0000-00-00', '18:00:00.0', 3, 3, 1, 'Kimia', 'Andi Bertoli', 'siswa1'),
-(4, '05/11/2021', '18:16', 1, 1, 1, 'Biologi', 'Hadian Syafiar', 'siswa1'),
-(5, '05/30/2021', '18:18', 12, 3, 15, 'Biologi', 'Andi Bertoli', 'akusiswa'),
-(6, '31/05/2021', '19:30', 13, 4, 1, 'fisiksa', 'Jimbon Mahardika', 'siswa1');
+(11, '05/06/2021', '09:00', 2, 2, 1, 'Biologi', 'Kusuma Ningrat', 'siswa1baru'),
+(12, '09/06/2021', '13:35', 3, 3, 15, 'Kimia', 'Baringin', 'kamusiswa'),
+(13, '09/06/2021', '19:01', 3, 3, 15, 'Matematika', 'Baringin', 'kamusiswa');
 
 -- --------------------------------------------------------
 
@@ -117,11 +112,9 @@ CREATE TABLE `matpel` (
 
 INSERT INTO `matpel` (`idMatpel`, `namaMatpel`, `idGuru`, `namaGuru`) VALUES
 (1, 'Biologi', 1, 'Hadian Syafiar'),
-(2, 'Matematika', 2, 'Kusuma Ningrat'),
-(3, 'Kimia', 3, 'Andi Bertoli'),
-(11, 'fisiksa', 27, 'asdasdas'),
-(12, 'Biologi', 3, 'Andi Bertoli'),
-(13, 'fisiksa', 4, 'Jimbon Mahardika');
+(2, 'Biologi', 2, 'Kusuma Ningrat'),
+(3, 'Matematika', 3, 'Baringin'),
+(12, 'Fisika', 3, 'Baringin');
 
 -- --------------------------------------------------------
 
@@ -144,8 +137,10 @@ CREATE TABLE `siswa` (
 --
 
 INSERT INTO `siswa` (`idSiswa`, `namaSiswa`, `noHp`, `alamat`, `email`, `username`, `password`) VALUES
-(1, 'siswa1', '08126372192', 'alamat1', 'siswa1@gmail.com', 'siswa1', '123456'),
-(15, 'akusiswa', '081023123', 'dimana aja', 'siswa123@asd.com', 'siswa123', '$2y$10$JTJ14xgwYkKkbfK3irFrx.eow.YBKfEj5vcOEPznReazmGFcR1cpe');
+(1, 'siswa1baru', '08126372192999', 'alamat1baru', 'siswa1baru@gmail.com', 'siswa1baru', '123456'),
+(15, 'kamusiswa', '081023123', 'dimana aja', 'siswa123@asd.com', 'siswa123', '$2y$10$JTJ14xgwYkKkbfK3irFrx.eow.YBKfEj5vcOEPznReazmGFcR1cpe'),
+(16, 'ilham', '08123123', 'pkp', 'henry@', 'asdd', '$2y$10$AfKDdbnYDhlzab1IQDb.1eqcZoZyrTQOUm1kpjlDZbk/OyXo0bseO'),
+(17, 'siswa', '08123123', 'pkp', 'siswa@gmail.com', 'siswa', '$2y$10$ynY9NiA7rqZn2O.XsJP9ienYdGpBLOfdXWMatZzrJO9uwdHAjYx6K');
 
 --
 -- Indexes for dumped tables
@@ -193,31 +188,31 @@ ALTER TABLE `siswa`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `idAdmin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idAdmin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `guru`
 --
 ALTER TABLE `guru`
-  MODIFY `idGuru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `idGuru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `jadwal`
 --
 ALTER TABLE `jadwal`
-  MODIFY `idJadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idJadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `matpel`
 --
 ALTER TABLE `matpel`
-  MODIFY `idMatpel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `idMatpel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `idSiswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `idSiswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Constraints for dumped tables
